32705728064798d57a7f59b00d9925f7703e2b52
Yakiv Lobach <lobachyakiv@gmail.com>
2024-07-23 13:09:54 +0300

    Washing Station_first_change add Annotation

--- app/main.py
+++ app/main.py
@@ -24,26 +24,23 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars: List) -> float:
+    def serve_cars(self, cars: List[Car]) -> float:
         total_income = 0.0
         for car in cars:
             if car.clean_mark < self.clean_power:
-                income = self.wash_single_car(car)
-                total_income += income
+                total_income += self.wash_single_car(car)
         return round(total_income, 1)
 
     def calculate_washing_price(self, car: Car) -> float:
         if not isinstance(car, Car):
             raise ValueError("The argument must be a Car instance")
-        if car.clean_mark >= self.clean_power:
-            return 0.0
-        washing_price = (
+
+        return round(
             car.comfort_class
             * (self.clean_power - car.clean_mark)
             * self.average_rating
-            / self.distance_from_city_center
+            / self.distance_from_city_center, 1
         )
-        return round(washing_price, 1)
 
     def wash_single_car(self, car: Car) -> float:
         if not isinstance(car, Car):
@@ -53,15 +50,13 @@ class CarWashStation:
             price = self.calculate_washing_price(car)
             car.clean_mark = self.clean_power
             return price
-        return 0.0
 
     def rate_service(self, new_rating: float) -> None:
         if not (1.0 <= new_rating <= 5.0):
             raise ValueError("new_rating must be between 1.0 and 5.0")
 
-        total_ratings = self.average_rating * self.count_of_ratings
-        new_total_ratings = total_ratings + new_rating
         self.count_of_ratings += 1
         self.average_rating = round(
-            new_total_ratings / self.count_of_ratings, 1
+            ((self.average_rating * (self.count_of_ratings - 1)) + new_rating)
+            / self.count_of_ratings, 1
         )
